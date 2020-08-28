176fd990706df8cc8b7ff499feedbd13d4c06f3f
Daw-Chih Liou <daw-chih.liou@trivago.com>
2020-08-28 20:15:02 +0200

    fix(ci.yml): configure release plugins

--- release.config.js
+++ release.config.js
@@ -5,6 +5,19 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+        tarballDir: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
     '@semantic-release/git',
   ],
   preset: 'angular',
