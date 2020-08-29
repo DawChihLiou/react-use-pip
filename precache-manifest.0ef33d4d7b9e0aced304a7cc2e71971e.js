f562ed5fcc1e77d8585f21e3ec322476f3b270ef
Daw-Chih Liou <daw-chih.liou@trivago.com>
2020-08-29 09:48:09 +0200

    Updates

---
+++ precache-manifest.0ef33d4d7b9e0aced304a7cc2e71971e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5166d4c3787a6d455a5dc4d2542e9cdb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54c96f82ab7955be0377",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "91c90bd8bf19938eac48",
+    "url": "./static/js/2.19dfb36e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.19dfb36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c96f82ab7955be0377",
+    "url": "./static/js/main.a803075c.chunk.js"
+  },
+  {
+    "revision": "e5e18c58f1c69f4437a2",
+    "url": "./static/js/runtime-main.fe2665be.js"
+  }
+]);
