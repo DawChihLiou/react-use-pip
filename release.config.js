830c23cacca5ed6bab1c3133ba7a32e6e7e22ef5
Daw-Chih Liou <daw-chih.liou@trivago.com>
2020-09-03 14:29:45 +0200

    fix(ci.yml): remove duplicated npm publish in release config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     [
       '@semantic-release/npm',
       {
